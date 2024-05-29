1e85de8b0b6dfac46bc5d87a26c4e1757904676f
BIRZHAN <birzhan981@gmail.com>
2024-05-29 08:32:57 +0500

    Done

--- mysite/requestdataapp/views.py
+++ mysite/requestdataapp/views.py
@@ -2,6 +2,8 @@ from django.core.files.storage import FileSystemStorage
 from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render
 
+from .forms import UserBioForm, UploadFileForm
+
 
 def process_get_view(request: HttpRequest) -> HttpResponse:
     a = request.GET.get("a", "")
@@ -16,18 +18,30 @@ def process_get_view(request: HttpRequest) -> HttpResponse:
 
 
 def user_form(request: HttpRequest) -> HttpResponse:
-    return render(request, "requestdataapp/user-bio-form.html")
+    context = {
+        "form": UserBioForm(),
+    }
+    return render(request, "requestdataapp/user-bio-form.html", context=context)
 
 
 def handle_file_uoload(request: HttpRequest) -> HttpResponse:
-    if request.method == "POST" and request.FILES.get("myfile"):
-        myfile = request.FILES["myfile"]
 
-        max_size_bytes = 1048576
-        if myfile.size > max_size_bytes:
-            return HttpResponseBadRequest("The file size exceeds the limit (1 MB).")
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            # myfile = request.FILES["myfile"]
+            myfile = form.cleaned_data["file"]
+            max_size_bytes = 1048576
+            if myfile.size > max_size_bytes:
+                return HttpResponseBadRequest("The file size exceeds the limit (1 MB).")
 
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        print("saved file", filename)
-    return render(request, "requestdataapp/file-upload.html")
+            fs = FileSystemStorage()
+            filename = fs.save(myfile.name, myfile)
+            print("saved file", filename)
+    else:
+        form = UploadFileForm()
+
+    context= {
+        "form": form,
+    }
+    return render(request, "requestdataapp/file-upload.html", context=context)
