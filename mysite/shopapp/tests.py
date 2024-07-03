c5b7374ac321abe32b2de2d8e6bcbc3e55d1408b
BIRZHAN <birzhan981@gmail.com>
2024-07-03 13:51:58 +0500

    ready comment

--- mysite/shopapp/tests.py
+++ mysite/shopapp/tests.py
@@ -51,97 +51,97 @@ class OrderDetailViewTestCase(TestCase):
 
 
 
-# class AddTwoNumbersTestCase(TestCase):
-#     def test_add_two_numbers(self):
-#         result = add_two_numbers(5, 3)
-#         self.assertEqual(result, 5)
+class AddTwoNumbersTestCase(TestCase):
+    def test_add_two_numbers(self):
+        result = add_two_numbers(5, 3)
+        self.assertEqual(result, 5)
 
 
-# class CreateProductViewTestCase(TestCase):
-#
-#     def setUp(self):
-#         # Создание и аутентификация пользователя
-#         self.user = User.objects.create_user(username='testuser', password='testpassword')
-#         self.user.user_permissions.add(Permission.objects.get(codename='add_product'))
-#         self.client.login(username='testuser', password='testpassword')
-#         self.product_name = ''.join(choices(ascii_letters, k=10))
-#         Product.objects.create(name=self.product_name).delete()
-#
-#     def test_create_product(self):
-#         response = self.client.post(
-#             reverse("shopapp:product_create"),
-#             {
-#                 'name': self.product_name,
-#                 'price': '123.45',
-#                 'description': 'A good table',
-#                 'discount': '10'
-#
-#             }
-#         )
-#
-#         self.assertRedirects(response, reverse("shopapp:products_list"))
-#         self.assertTrue(
-#             Product.objects.filter(name=self.product_name).exists()
-#         )
+class CreateProductViewTestCase(TestCase):
+
+    def setUp(self):
+        # Создание и аутентификация пользователя
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.user.user_permissions.add(Permission.objects.get(codename='add_product'))
+        self.client.login(username='testuser', password='testpassword')
+        self.product_name = ''.join(choices(ascii_letters, k=10))
+        Product.objects.create(name=self.product_name).delete()
+
+    def test_create_product(self):
+        response = self.client.post(
+            reverse("shopapp:product_create"),
+            {
+                'name': self.product_name,
+                'price': '123.45',
+                'description': 'A good table',
+                'discount': '10'
+
+            }
+        )
+
+        self.assertRedirects(response, reverse("shopapp:products_list"))
+        self.assertTrue(
+            Product.objects.filter(name=self.product_name).exists()
+        )
 
 
-# class ProductDetailViewTestCase(TestCase):
-#     @classmethod
-#     def setUpClass(cls):
-#         cls.product = Product.objects.create(name='Product 1')
-#
-#     @classmethod
-#     def tearDownClass(cls):
-#         cls.product.delete()
-#
-#     def test_get_product_and_check_content(self):
-#         response = self.client.get(
-#             reverse('shopapp:product_details', kwargs={'pk': self.product.pk})
-#         )
-#         self.assertContains(response, self.product.name)
+class ProductDetailViewTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.product = Product.objects.create(name='Product 1')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.product.delete()
+
+    def test_get_product_and_check_content(self):
+        response = self.client.get(
+            reverse('shopapp:product_details', kwargs={'pk': self.product.pk})
+        )
+        self.assertContains(response, self.product.name)
 
 
-# class ProductListViewTest(TestCase):
-#     fixtures = [
-#         'products-fixtures.json',
-#     ]
-#
-#     def test_products(self):
-#         response = self.client.get(reverse('shopapp:products_list'))
-#         self.assertQuerysetEqual(
-#             qs=Product.objects.filter(archived=False).all(),
-#             values=(p.pk for p in response.context['products']),
-#             transform=lambda p: p.pk,
-#         )
-#         self.assertTemplateUsed(response, 'shopapp/products-list.html')
+class ProductListViewTest(TestCase):
+    fixtures = [
+        'products-fixtures.json',
+    ]
 
-#
+    def test_products(self):
+        response = self.client.get(reverse('shopapp:products_list'))
+        self.assertQuerysetEqual(
+            qs=Product.objects.filter(archived=False).all(),
+            values=(p.pk for p in response.context['products']),
+            transform=lambda p: p.pk,
+        )
+        self.assertTemplateUsed(response, 'shopapp/products-list.html')
 
 
-# class ProductExportViewTestCase(TestCase):
-#     fixtures = {
-#         'products-fixtures.json',
-#     }
-#
-#     def test_get_products_view(self):
-#         response = self.client.get(reverse('shopapp:products-export'))
-#         self.assertEqual(response.status_code, 200)
-#         products = Product.objects.order_by("pk").all()
-#         expected_data = [
-#             {
-#                 'pk': product.pk,
-#                 'name': product.name,
-#                 'price': str(product.price),
-#                 'archived': product.archived,
-#             }
-#             for product in products
-#         ]
-#
-#         products_data = response.json()
-#         self.assertEqual(
-#             products_data['products'],
-#             expected_data
-#         )
+
+
+class ProductExportViewTestCase(TestCase):
+    fixtures = {
+        'products-fixtures.json',
+    }
+
+    def test_get_products_view(self):
+        response = self.client.get(reverse('shopapp:products-export'))
+        self.assertEqual(response.status_code, 200)
+        products = Product.objects.order_by("pk").all()
+        expected_data = [
+            {
+                'pk': product.pk,
+                'name': product.name,
+                'price': str(product.price),
+                'archived': product.archived,
+            }
+            for product in products
+        ]
+
+        products_data = response.json()
+        self.assertEqual(
+            products_data['products'],
+            expected_data
+        )
 
 
 
